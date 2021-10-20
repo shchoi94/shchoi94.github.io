b54f3586d58c14ec4372c8ded8f2cc1628f412a0
shchoi94 <hyuklassic@gmail.com>
2021-10-20 23:30:01 +0900

    Set name

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Message Queue",
+        "excerpt":"MQ란? MQ (Message Queue) 란 메시지 기반의 미들웨어로 메시지를 이용하여 여러 어플리케이션, 시스템, 서비스들을 연결해주는 솔루션이다. MOM(Message Oriented Middleware) 를 구현한 솔루션으로 비동기 메시지를 사용하는 서비스들 사이에서 데이터를 교환해주는 역할을 한다. Producer(sender) 가 메시지를 큐에 전송하면 Consumer(receiver) 가 처리하는 방식으로, producer 와 consumer 에 message 프로세스가 추가되는 것이 특징이다. 메시지를...","categories": [],
+        "tags": [],
+        "url": "/Message-Queue/",
+        "teaser": null
+      }]
